05bb4d59b87652fbb3fd734695f5fe058d0654f5
mackmeggie <mack.meggie13@gmail.com>
2022-08-05 14:24:45 -0600

    import css styling, set initial load functions

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import './style.css';
+
+
+function buildNav() {
+
+    const element = document.querySelector('div#content');
+
+    const nav = document.createElement('nav');
+    element.appendChild(nav);
+
+    const homeTab = document.createElement('button');
+    homeTab.classList.add('button-nav')
+    homeTab.id="tab1";
+    homeTab.textContent="Home";
+    homeTab.addEventListener('click', (e) => {
+        if (e.target.classList.contains("active")) return;
+        setActiveButton(homeTab);
+        homeLoad();
+    })
+    nav.appendChild(homeTab);
+
+    const menuTab = document.createElement('button');
+    menuTab.classList.add('button-nav')
+    menuTab.id="tab2";
+    menuTab.textContent="Menu";
+    menuTab.addEventListener('click', (e) => {
+        if (e.target.classList.contains('active')) return;
+        setActiveButton(menuTab);
+        menuLoad();
+    });
+    nav.appendChild(menuTab);
+
+    const contactTab = document.createElement('button');
+    contactTab.classList.add('button-nav')
+    contactTab.id="tab3";
+    contactTab.textContent="Contact";
+    nav.appendChild(contactTab);
+
+    return buildNav;
+}
+
+function setActiveButton(button) {
+    const buttons = document.querySelectorAll(".button-nav");
+  
+    buttons.forEach((button) => {
+      if (button !== this) {
+        button.classList.remove("active");
+      }
+    });
+  
+    button.classList.add("active");
+  }
+
+function buildBackground() {
+    const element = document.querySelector('div#content');
+
+    const contentContainer = document.createElement('div');
+    contentContainer.id="contentContainer";
+    element.appendChild(contentContainer);
+}
+
+  buildNav();
+  buildBackground();
